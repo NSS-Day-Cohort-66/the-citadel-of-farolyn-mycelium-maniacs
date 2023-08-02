5255df6334f94a92fe52f55a07ed20a310cadf54
Nathan Chevalier <cerberusgfx@gmail.com>
2023-08-02 11:02:39 -0500

    Moved archetypes array to global scope, added greeting

--- scripts/TypeList.js
+++ scripts/TypeList.js
@@ -1,11 +1,14 @@
 import { getArchetypes } from "./database.js";
+
+const archetypes = getArchetypes();
+
 document.addEventListener("click", (clickEvent) => {
   const typeClicked = clickEvent.target;
 
   if (typeClicked.dataset.type === "type") {
     const detailsHtml = `
     <div>
-      <h4>Greeting:</h4>
+      <h4>Greeting:${archetypes.greeting}</h4>
       <h4>School:</h4>
       <h4>Study Book:</h4>
     </div>`;
@@ -13,7 +16,6 @@ document.addEventListener("click", (clickEvent) => {
 });
 
 export const TypeList = () => {
-  const archetypes = getArchetypes();
   let headerHtml = `<div id="archetypes_container" class="content_block">`;
   headerHtml += `<h3>Magician Types:</h3>`;
   headerHtml += `<ul>`;
